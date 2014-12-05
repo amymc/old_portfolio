f423f020e0461fe35606b938183542da9fd84a16
amymc <amy.mccarthy2@mail.dcu.ie>
2014-12-05 12:18:45 -0500

    minor script changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,27 @@
-
-//initialize skrollr.js - parallax scrolling plugin
-var s = skrollr.init();
-//initiate skrollr-menu - plugin to allow hash navigation on mobile
-skrollr.menu.init(s);
-
-//Bootstrap scrollspy - highlights navbar targets based on scroll position
-$('body').scrollspy({ target: '.navbar' });
-
-//initialize lightbox plugin
 $(document).ready(function(){
+
+    //initialize lightbox plugin
     $('.lightbox').nivoLightbox({
         effect: 'fadeScale'
     });
 
+    //initialize skrollr.js - parallax scrolling plugin
+    var s = skrollr.init();
+    //initiate skrollr-menu - plugin to allow hash navigation on mobile
+    skrollr.menu.init(s);
+
+    //Bootstrap scrollspy - highlights navbar targets based on scroll position
+    $('body').scrollspy({ target: '.navbar' });
+
+    //replicate hover on touch devices
     $('.experience-info-front').on('touchstart', function(e) {
       e.preventDefault();
       $(this).addClass('hover_effect');
     });
-});
 
-//hide nav menu on clicking nav li item
-$('.nav li a').on('click',function(){
-    $('.navbar-collapse.in').collapse('hide');
+    //hide nav menu on clicking nav li item
+    $('.nav li a').on('click',function(){
+      $('.navbar-collapse.in').collapse('hide');
+    });
+    
 });
